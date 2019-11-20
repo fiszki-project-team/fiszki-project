1dd74dbefc7c9c69ed7399f5f7b81c2c777bee6a
Kinga Tokarz <kinga8tokarz@gmail.com>
2019-11-20 14:47:48 +0100

    Initial Commit, Spring boot project generated

---
+++ api/src/main/java/com/fiszki/fiszkiproject/FiszkiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fiszki.fiszkiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiszkiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiszkiProjectApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/fiszki/fiszkiproject/FiszkiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiszki.fiszkiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiszkiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
