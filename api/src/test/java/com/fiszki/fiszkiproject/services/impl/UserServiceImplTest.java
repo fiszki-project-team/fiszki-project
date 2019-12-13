4ec569f157d7618dc82bcbcfae06bde6661ed64a
Michal Jewczuk <michal.jewczuk@outlook.com>
2019-12-13 07:40:17 +0100

    Changed all test in changePassword to user mocked service

--- api/src/test/java/com/fiszki/fiszkiproject/services/impl/UserServiceImplTest.java
+++ api/src/test/java/com/fiszki/fiszkiproject/services/impl/UserServiceImplTest.java
@@ -32,49 +32,48 @@ import com.fiszki.fiszkiproject.repositories.UserRepository;
 import com.fiszki.fiszkiproject.services.UserService;
 import com.fiszki.fiszkiproject.validators.UserValidator;
 
-
 @SpringBootTest
 @Transactional
 public class UserServiceImplTest {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Nested
 	@DisplayName("get user")
 	class GetUser {
-		
+
 		@Test
 		@DisplayName("should return list of all users")
 		public void shouldReturnListOfAllUsers() {
 			List<UserBasicInfoDto> users = userService.getAllUsers();
 			List<Long> userIds = users.stream().map(u -> u.getId()).collect(Collectors.toList());
-			
+
 			assertThat(userIds).containsExactlyInAnyOrder(1L, 2L, 3L);
 		}
-		
+
 		@Test
 		@DisplayName("should return user with valid id")
 		public void shouldReturnUserWithValidId() {
 			UserBasicInfoDto user = userService.getUserById(2L);
-			
+
 			assertThat(user.getDisplayName()).isEqualTo("user_2");
 			assertThat(user.getId()).isEqualTo(2L);
 		}
-		
+
 		@Test
 		@DisplayName("should return null with invalid id")
 		public void shouldReturnNullWhenNoUserWithGivenId() {
 			UserBasicInfoDto user = userService.getUserById(-1L);
-			
+
 			assertThat(user).isNull();
 		}
 	}
-	
+
 	@Nested
 	@DisplayName("change display name")
 	class ChangeDisplayName {
-		
+
 		@Test
 		@DisplayName("should return true for valid name for valid user")
 		public void shouldSuccessfullyChangeDisplayName() throws ValidatorException {
@@ -82,113 +81,112 @@ public class UserServiceImplTest {
 			String newDisplayName = "validName";
 			UserNameChangeDto dto = new UserNameChangeDto(userId, newDisplayName);
 			UserBasicInfoDto beforeChange = userService.getUserById(userId);
-			
+
 			boolean result = userService.changeDisplayName(dto);
 			UserBasicInfoDto afterChange = userService.getUserById(userId);
-			
+
 			assertThat(result).isTrue();
 			assertThat(afterChange.getDisplayName()).isEqualTo(newDisplayName);
 			assertThat(afterChange.getDisplayName()).isNotEqualTo(beforeChange.getDisplayName());
 		}
-		
+
 		@Test
 		@DisplayName("should return false for valid name and invalid user")
 		public void shouldReturnFalseWhenUserIdIsNotValid() throws ValidatorException {
 			Long userId = -1L;
 			String newDisplayName = "validName";
 			UserNameChangeDto dto = new UserNameChangeDto(userId, newDisplayName);
-			
+
 			boolean result = userService.changeDisplayName(dto);
-			
+
 			assertThat(result).isFalse();
 		}
-		
+
 		@Test
 		@DisplayName("should throw exception when invalid name")
 		public void shouldThrowExceptionWhenNewNameIsInvalid() throws ValidatorException {
 			Long userId = 1L;
 			String newDisplayName = "r    ";
 			UserNameChangeDto dto = new UserNameChangeDto(userId, newDisplayName);
-			
-			assertThatThrownBy(() -> userService.changeDisplayName(dto))
-				.isInstanceOf(ValidatorException.class)
-				.hasMessage(Errors.DISPLAY_NAME_TOO_SHORT.toString());
+
+			assertThatThrownBy(() -> userService.changeDisplayName(dto)).isInstanceOf(ValidatorException.class)
+					.hasMessage(Errors.DISPLAY_NAME_TOO_SHORT.toString());
 		}
-		
+
 		@Test
 		@DisplayName("should throw exception when name already taken")
 		public void shouldThrowExceptionWhenNewNameAlreadyTaken() throws ValidatorException {
 			Long userId = 2L;
 			String newDisplayName = "user_1";
 			UserNameChangeDto dto = new UserNameChangeDto(userId, newDisplayName);
-			
-			assertThatThrownBy(() -> userService.changeDisplayName(dto))
-				.isInstanceOf(ValidatorException.class)
-				.hasMessage(Errors.DISPLAY_NAME_ALREADY_TAKEN.toString());
+
+			assertThatThrownBy(() -> userService.changeDisplayName(dto)).isInstanceOf(ValidatorException.class)
+					.hasMessage(Errors.DISPLAY_NAME_ALREADY_TAKEN.toString());
 		}
 	}
-	
+
 	@Nested
 	@DisplayName("change password")
 	class ChangePassword {
-		
+
 		UserPasswordChangeDto dto;
-		
+
 		@Mock
 		UserValidator validator;
-		
+
 		@Mock
 		UserRepository repository;
-		
+
 		@InjectMocks
 		UserServiceImpl mockedService;
-		
+
 		@BeforeEach
 		void setUp() {
 			dto = new UserPasswordChangeDto(1L, "test", "Test1234");
+			when(repository.findById(any(Long.class))).thenReturn(Optional.of(new User()));
 		}
-		
+		 
+
 		@Test
 		@DisplayName("should return true when valid password and user id")
 		public void changeValidPassword() throws ValidatorException {
-	
-			boolean result = userService.changePassword(dto);
+
+			boolean result = mockedService.changePassword(dto);
 			
 			assertThat(result).isTrue();
 		}
-		
+
 		@Test
 		@DisplayName("should return false when valid password and invalid user id")
 		public void changeInvalidUserId() throws ValidatorException {
-			dto.setId(-1L);
-			
-			boolean result = userService.changePassword(dto);
-			
+			when(repository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+			boolean result = mockedService.changePassword(dto);
+
 			assertThat(result).isFalse();
 		}
-		
+
 		@Test
 		@DisplayName("should throw exception when invalid new password")
 		public void changeInvalidNewPassword() throws ValidatorException {
-			dto.setNewPassword("12345678");
-			
-			assertThatThrownBy(() -> userService.changePassword(dto))
-				.isInstanceOf(UserValidatorException.class)
-				.hasMessage(Errors.PASSWORD_HAS_NO_CAPITAL_LETTERS.toString());
+			when(validator.validatePassword(any(String.class)))
+					.thenThrow(new UserValidatorException(Errors.PASSWORD_HAS_NO_CAPITAL_LETTERS));
+
+			assertThatThrownBy(() -> mockedService.changePassword(dto)).isInstanceOf(UserValidatorException.class)
+					.hasMessage(Errors.PASSWORD_HAS_NO_CAPITAL_LETTERS.toString());
 		}
-		
+
 		@Test
 		@DisplayName("should throw exception when invalid old password")
 		public void changeInvalidOldPassword() throws ValidatorException {
-			when(repository.findById(any(Long.class))).thenReturn(Optional.of(new User()));
+			when(validator.validatePassword(any(String.class))).thenReturn(true);
 			when(validator.comparePasswords(any(), any(String.class)))
-				.thenThrow(new AuthValidatorException(Errors.INVALID_PASSWORD));
-			
-			assertThatThrownBy(() -> mockedService.changePassword(dto))
-				.isInstanceOf(AuthValidatorException.class)
-				.hasMessage(Errors.INVALID_PASSWORD.toString());
+					.thenThrow(new AuthValidatorException(Errors.INVALID_PASSWORD));
+
+			assertThatThrownBy(() -> mockedService.changePassword(dto)).isInstanceOf(AuthValidatorException.class)
+					.hasMessage(Errors.INVALID_PASSWORD.toString());
 		}
-		
+
 	}
 
 }
