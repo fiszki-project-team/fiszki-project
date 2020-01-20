877e9b2943d9908c03753d9e53ea77fad936f7cb
Kinga Tokarz <kinga8tokarz@gmail.com>
2020-01-20 14:46:44 +0100

    TagRepoImplTests modified - now parametrized & nested tests implemented

--- api/src/test/java/com/fiszki/fiszkiproject/repositories/impl/TagRepositoryImplTest.java
+++ api/src/test/java/com/fiszki/fiszkiproject/repositories/impl/TagRepositoryImplTest.java
@@ -7,7 +7,11 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,23 +21,38 @@ import com.fiszki.fiszkiproject.repositories.TagRepository;
 @SpringBootTest
 @Transactional
 class TagRepositoryImplTest {
-	
+
 	@Autowired
 	private TagRepository tagRepository;
 
-	@Test
-	void shouldReturnListOfAllTagsPerUser() {
-		List<Tag> tags = tagRepository.findTagsByUserId(1L);
-		
-		assertEquals(2, tags.size());
-		assertEquals("holidays", tags.get(0).getDisplayName());
-		assertEquals("work", tags.get(1).getDisplayName());
+	@Nested
+	@DisplayName("Tags list - get by User Id")
+	class FindTagsByUserId {
+		@ParameterizedTest
+		@DisplayName("returns an empty List when User does not exist in DataBase")
+		@NullSource
+		@ValueSource(longs = { -3L, 0L, 2000L })
+		public void returnEmptyListWhenNoUserInDataBase(Long Id) {
+			assertThat(tagRepository.findTagsByUserId(Id)).isEmpty();
+		}
+
+		@ParameterizedTest
+		@DisplayName("returns an empty List when User has zero Tags")
+		@ValueSource(longs = { 3L })
+		public void returnEmptyListWhenUserHasZeroTags(Long Id) {
+			assertThat(tagRepository.findTagsByUserId(Id)).isEmpty();
+		}
+
+		@ParameterizedTest
+		@DisplayName("returns a list of Tags for User with given Id")
+		@ValueSource(longs = { 1L })
+		public void returnListOfTagsForUser(Long Id) {
+			List<Tag> tags = tagRepository.findTagsByUserId(1L);
+
+			assertThat(tags).isNotEmpty().isNotNull();
+			assertEquals(2, tags.size());
+			assertEquals("holidays", tags.get(0).getDisplayName());
+		}
 	}
 
-	@Test
-	void shouldReturnEmptyListWhenNoUserWithGivenId() {
-		List<Tag> tags = tagRepository.findTagsByUserId(-3L);
-		
-		assertThat(tags.isEmpty());
-	}
 }
